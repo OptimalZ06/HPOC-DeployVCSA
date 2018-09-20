dfb114ac1113e971e226e952b00551104ea563dd
Troy <troy.g.ward@gmail.com>
2018-09-20 09:40:59 -0500

    Many changes

--- CreateVCSA.ps1
+++ CreateVCSA.ps1
@@ -1,10 +1,109 @@
-param($HPOCPassword, $POC, $ESXiHost)
+Param(
+[Parameter(Mandatory=$false)]
+$HPOCPassword,
+[Parameter(Mandatory=$false)]#[ValidateSet("Perfect","Value","Export","Optimized","RotoGrinders")]
+$POC,
+[Parameter(Mandatory=$false)]#[ValidateSet("YAHOO","FANDUEL","DRAFTKINGS")]
+$ESXiHost)
 
 # Create a banner because we want it to be pretty
 function Banner {
 	write-host "`n---------------------------------" -f yellow
 	write-host "Nutani" -f green -nonewline; write-host "x " -f yellow -nonewline; write-host "VCSA Deployment Kit" -f green -nonewline;
 	write-host "`n---------------------------------`n" -f yellow
+}
+
+function Get-Info($HPOCPass, $POC, $ESXiHost) {
+	# Draw banner
+	cls
+	Banner
+
+	write-host "***** READ THE PROMPTS CAREFULLY *****`n" -f red
+
+	# POC Number
+	write-host "Your POC Reservation (" -f yellow -nonewline; write-host "e.g. POC086" -f white -nonewline; write-host "): " -f yellow -nonewline
+	$POC = read-host
+
+	# Set VCSA IP Address
+	if($POC -like "POC0*" ) {
+		$vcsaIP = "10.21." + $POC.Substring($POC.Length - 2) + ".40"
+		$Gateway = "10.21." + $POC.Substring($POC.Length - 2) + ".1"
+		$ESXiGuess = "10.21." + $POC.Substring($POC.Length - 2) + ".25"
+		$HPOC = $POC.Substring($POC.Length - 2)
+	} else {
+		$vcsaIP = "10.21." + $POC.Substring($POC.Length - 3) + ".40"
+		$Gateway = "10.21." + $POC.Substring($POC.Length - 3) + ".1"
+		$ESXiGuess = "10.21." + $POC.Substring($POC.Length - 3) + ".25"
+		$HPOC = $POC.Substring($POC.Length - 3)
+	}
+
+	# ESXi Hosts
+	write-host "Is your ESXi Host " -f yellow -nonewline; write-host $ESXiGuess -f white -nonewline; write-host " (Y/n)? " -f yellow -nonewline
+	$ESXi = read-host
+    switch($ESXi) {
+		Y { $ESXi = $ESXiGuess }
+		N {
+			write-host "Your ESXi Host (" -f yellow -nonewline; write-host "e.g. 10.21.86.25" -f white -nonewline; write-host "): " -f yellow -nonewline
+			$ESXi = read-host
+		}
+		Default { $ESXi = $ESXiGuess }
+	}
+
+	# Set HPOC Password if it wasn't already set
+	if(!$HPOCPass) {
+		write-host "HPOC Password: " -f yellow -nonewline
+		$HPOCPass = read-host
+	}
+
+	# TODO: Check Login before sending deployment package
+
+	# TODO: Ask for verification
+
+	Get-Menu $vcsaIP $Gateway $ESXiGuess $HPOC $HPOCPass $ESXi
+}
+
+function Get-Menu($vcsaIP, $Gateway, $ESXiGuess, $HPOC, $HPOCPass, $ESXi) {
+	write-host $vcsaIP $Gateway $ESXiGuess $HPOC $HPOCPass $ESXi
+}
+
+# Menu
+function Get-Menu($vcsaIP, $Gateway, $ESXiGuess, $HPOC, $HPOCPass, $ESXi) {
+	# Draw banner
+	cls
+	Banner
+
+	# Static Menu
+	write-host -nonewline; write-host "1. " -f yellow -nonewline; write-host "Setup PE"
+	write-host -nonewline; write-host "2. " -f yellow -nonewline; write-host "Deploy VCSA"
+	write-host -nonewline; write-host "3. " -f yellow -nonewline; write-host "Deploy Xtract & Test VMs"
+	write-host -nonewline; write-host "4. " -f yellow -nonewline; write-host "Deploy Calm"
+
+	# Exit
+	write-host -nonewline; write-host "`n0. " -f yellow -nonewline; write-host "Exit"
+
+	write-host "`nPlease select an option (0-4): "  -f yellow -NoNewline
+	$global:options_home = read-host
+
+	if($options_home -eq 1) {
+		# Setup PE TODO: Fix hardcoded PW
+		Setup-PrismElement $HPOC nutanix/4u $HPOCPass
+	} elseif($options_home -eq 2) {
+		Setup-VCSA $vcsaIP $Gateway $ESXiGuess $HPOC $HPOCPass $ESXi
+	} elseif($options_home -eq 3) {
+		Setup-Xtract
+	} elseif($options_home -eq 4) {
+		Setup-Calm
+	} elseif($options_home -eq 0) {
+		cls
+		write-host -nonewline; write-host "`nBye!" -f yellow -nonewline;
+		sleep 2
+		exit
+	} else {
+		if(!$debugmode) { cls }
+		$global:currentfunction = $null;
+		#GeneralOptions($options_home)
+		Get-Menu 0
+	}
 }
 
 function Setup-PrismElement($HPOC, $PEPassword, $HPOCPassword) {
@@ -54,7 +153,7 @@ $certCallback = @"
   $Result = echo y | ./plink.exe nutanix@$HPOCIP -pw $PEPassword "~/prism/cli/ncli user reset-password user-name=admin password=$HPOCPassword"
 
   # Accept EULA
-	$EULAJson = '{"username": "SE with stage_calmhow.sh","companyName": "Nutanix","jobTitle": "SE"}'
+	$EULAJson = '{"username": "SE with CC+","companyName": "Nutanix","jobTitle": "SE"}'
 	$EULAAddress = "https://" + $HPOCIP + ":" + "9440/PrismGateway/services/rest/v1/eulas/accept"
 	$EULAResponse = Invoke-RestMethod $EULAAddress -Headers $Header -Method POST -Body $EULAJson -ContentType 'application/json'
 
@@ -65,56 +164,14 @@ $certCallback = @"
 
 	# TODO: Create storage container
 	# if(!(Get-NTNXContainer).Name -eq "VMware") { $CreateContainer = New-NTNXContainer -Name "VMware" }
+
+	$global:PEDeployed = $true
 }
 
-function New-DeployVCSA($HPOCPass, $POC, $ESXiHost) {
-	# Draw Banner
-	cls
-	Banner
-
-	write-host "***** READ THE PROMPTS CAREFULLY *****`n" -f red
-
-	# POC Number
-	write-host "Your POC Reservation (" -f yellow -nonewline; write-host "e.g. POC086" -f white -nonewline; write-host "): " -f yellow -nonewline
-	$POC = read-host
-
-	# Set VCSA IP Address
-	if($POC -like "POC0*" ) {
-		$vcsaIP = "10.21." + $POC.Substring($POC.Length - 2) + ".40"
-		$Gateway = "10.21." + $POC.Substring($POC.Length - 2) + ".1"
-		$ESXiGuess = "10.21." + $POC.Substring($POC.Length - 2) + ".25"
-		$HPOC = $POC.Substring($POC.Length - 2)
-	} else {
-		$vcsaIP = "10.21." + $POC.Substring($POC.Length - 3) + ".40"
-		$Gateway = "10.21." + $POC.Substring($POC.Length - 3) + ".1"
-		$ESXiGuess = "10.21." + $POC.Substring($POC.Length - 3) + ".25"
-		$HPOC = $POC.Substring($POC.Length - 3)
-	}
-
-	# ESXi Hosts
-	write-host "Is your ESXi Host " -f yellow -nonewline; write-host $ESXiGuess -f white -nonewline; write-host " (Y/n)? " -f yellow -nonewline
-	$ESXi = read-host
-    switch($ESXi) {
-		Y { $ESXi = $ESXiGuess }
-		N {
-			write-host "Your ESXi Host (" -f yellow -nonewline; write-host "e.g. 10.21.86.25" -f white -nonewline; write-host "): " -f yellow -nonewline
-			$ESXi = read-host
-		}
-		Default { $ESXi = $ESXiGuess }
-	}
-
-	# Set HPOC Password if it wasn't already set
-	if(!$HPOCPass) {
-		write-host "HPOC Password: " -f yellow
-		$HPOCPass = read-host
-	}
-
-	# Setup PE TODO: Fix hardcoded PW
-	Setup-PrismElement $HPOC nutanix/4u $HPOCPass
-
-	# TODO: Check Login before sending deployment package
-
-	# TODO: Ask for verification
+function Setup-VCSA($vcsaIP, $Gateway, $ESXiGuess, $HPOC, $HPOCPass, $ESXi) {
+	# Setup PE; Only if it hasn't been setup already
+	# TODO: Fix hardcoded PW
+	if(!$PEDeployed) { Setup-PrismElement $HPOC nutanix/4u $HPOCPass }
 
 	# Create JSON Package
 	$JSON = @{
@@ -152,4 +209,8 @@ function New-DeployVCSA($HPOCPass, $POC, $ESXiHost) {
 	sleep 600
 }
 
-New-DeployVCSA -HPOCPass $HPOCPassword -POC $POC -ESXiHost $ESXiHost
+if(!$HPOCPassword -or !$POC -or !$ESXiHost) {
+	Get-Menu
+} else {
+	Setup-VCSA -HPOCPass $HPOCPassword -POC $POC -ESXiHost $ESXiHost
+}
